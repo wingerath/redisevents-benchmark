507f3aa7df403bd5b414aad3d94dbe55d0d996d7
Wolfram Wingerath <wingerath@informatik.uni-hamburg.de>
2017-06-20 13:34:44 +0200

    .

--- cluster.sh
+++ cluster.sh
@@ -1,11 +1,11 @@
 
 for i in "$@"
-docker run \
-   -d \
-   --cpu-period=1000000 --cpu-quota=1000 \
-   -e constraint:server==manager \
-   -p $i:6379 \
-   --restart=always \
-   --name "redis$i" \
-    redis:3.2.9
+	docker run \
+	   -d \
+	   --cpu-period=1000000 --cpu-quota=1000 \
+	   -e constraint:server==manager \
+	   -p $i:6379 \
+	   --restart=always \
+	   --name "redis$i" \
+		redis:3.2.9
 done
