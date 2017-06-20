1d5d823306643042088db4e059794393325d4ea6
Wolfram Wingerath <wingerath@informatik.uni-hamburg.de>
2017-06-20 15:10:30 +0200

    .

--- cluster.sh
+++ cluster.sh
@@ -13,10 +13,10 @@ sleep 5
 for i in "$@"
 do
 docker run \
-   -d \
+   -it \
    --cpu-period=1000000 --cpu-quota=1000 \
    -e constraint:server==manager \
-   -v $(readlink -m redis{$i}.conf):/usr/local/etc/redis/redis.conf \
+   -v $(readlink -m redis$i.conf):/usr/local/etc/redis/redis.conf \
    -p $i:$i \
    --restart=always \
    --name redis$i \
