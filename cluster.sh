474485929c08b783e40145c6767563650499494a
Wolfram Wingerath <wingerath@informatik.uni-hamburg.de>
2017-06-20 13:15:43 +0200

    .

---
+++ cluster.sh
@@ -0,0 +1,20 @@
+for (( i=1; i <= {$@}; i++ )); do
+docker stop redis$i &
+done
+sleep 2
+
+for (( i=1; i <= {$@}; i++ )); do
+docker rm redis$i &
+done
+sleep 2
+
+for (( i=1; i <= {$@}; i++ )); do
+docker run \
+   -d \
+   --cpu-period=1000000 --cpu-quota=1000 \
+   -e constraint:server==manager \
+   -p $i:6379 \
+   --restart=always \
+   --name redis$i \
+    redis:3.2.9
+done
