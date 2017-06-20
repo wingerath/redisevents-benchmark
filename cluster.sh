c1cd8932536801d385e318f5ff581616a26fe16b
Wolfram Wingerath <wingerath@informatik.uni-hamburg.de>
2017-06-20 13:36:15 +0200

    .

--- cluster.sh
+++ cluster.sh
@@ -1,11 +1,20 @@
+for i in "$@"
+echo docker stop "redis$i" &
+done
+sleep 2
 
 for i in "$@"
-	docker run \
-	   -d \
-	   --cpu-period=1000000 --cpu-quota=1000 \
-	   -e constraint:server==manager \
-	   -p $i:6379 \
-	   --restart=always \
-	   --name "redis$i" \
-		redis:3.2.9
+echo docker rm "redis$i" &
+done
+sleep 2
+
+for i in "$@"
+echo docker run \
+   -d \
+   --cpu-period=1000000 --cpu-quota=1000 \
+   -e constraint:server==manager \
+   -p $i:6379 \
+   --restart=always \
+   --name "redis$i" \
+    redis:3.2.9
 done
